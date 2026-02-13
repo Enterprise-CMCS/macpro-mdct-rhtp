c7000590f00e4bc667ea6d32beb509bbcd0a8e76
Filipp Gorbunov <filippgorb@gmail.com>
2026-02-13 12:09:47 -0800

    ported over the rest of the styles

--- services/ui-src/src/styles/components/index.ts
+++ services/ui-src/src/styles/components/index.ts
@@ -1,9 +1,21 @@
+import accordionTheme from "./accordion";
 import buttonTheme from "./button";
+import dividerTheme from "./divider";
 import headingTheme from "./heading";
+import linkTheme from "./link";
+import listTheme from "./list";
 import modalTheme from "./modal";
+import tableTheme from "./table";
+import textTheme from "./text";
 
 export const components = {
+  Accordion: accordionTheme,
   Button: buttonTheme,
+  Divider: dividerTheme,
   Heading: headingTheme,
+  Link: linkTheme,
+  List: listTheme,
   Modal: modalTheme,
+  Table: tableTheme,
+  Text: textTheme,
 };
