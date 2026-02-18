1007c0586d520885bd3bb0e6a77d25a64438457a
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2026-02-18 12:06:16 -0500

    add general information page to report (#12)

--- services/app-api/forms/2026/rhtp/rhtp.ts
+++ services/app-api/forms/2026/rhtp/rhtp.ts
@@ -14,7 +14,40 @@ export const rhtpReportTemplate: ReportBase = {
   pages: [
     {
       id: "root",
-      childPageIds: ["sustainability-and-highlights", "review-submit"],
+      childPageIds: [
+        "general-information",
+        "sustainability-and-highlights",
+        "review-submit",
+      ],
+    },
+    {
+      id: "general-information",
+      title: "General Information",
+      type: PageType.Standard,
+      sidebar: true,
+      elements: [
+        {
+          type: ElementType.Header,
+          id: "general-information-header",
+          text: "General Information",
+        },
+        {
+          id: "contact-name",
+          type: ElementType.Textbox,
+          label: "Contact name",
+          required: true,
+          helperText:
+            "Enter a person's name or a position title for CMS to contact with questions about this report.",
+        },
+        {
+          type: ElementType.Textbox,
+          id: "contact-email",
+          label: "Contact email address",
+          required: true,
+          helperText:
+            "Enter an email address for the person or position above. Department or program-wide email addresses are allowed.",
+        },
+      ],
     },
     {
       id: "sustainability-and-highlights",
