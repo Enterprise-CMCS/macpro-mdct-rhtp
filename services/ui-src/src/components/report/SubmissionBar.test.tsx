50867ea7b22df47bdd8e6055e9763e6ee99ac38d
ailZhou <aileen.zhou@coforma.io>
2026-04-22 16:33:35 -0400

    Update SubmissionBar.test.tsx

--- services/ui-src/src/components/report/SubmissionBar.test.tsx
+++ services/ui-src/src/components/report/SubmissionBar.test.tsx
@@ -1,12 +1,15 @@
 import { useStore } from "utils";
 import { MockedFunction } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { SubmissionBar } from "./SubmissionBar";
 import { mockStateUserStore } from "utils/testing/setupTest";
+import userEvent from "@testing-library/user-event";
 
 vi.mock("utils/state/useStore");
 const mockedUseStore = useStore as unknown as MockedFunction<typeof useStore>;
 const mockSetModalComponent = vi.fn();
+const mockSetModalOpen = vi.fn();
+const mockCreateZip = vi.fn();
 
 const mockNavigate = vi.fn();
 vi.mock("react-router", async (importOriginal) => ({
@@ -14,35 +17,86 @@ vi.mock("react-router", async (importOriginal) => ({
   useNavigate: () => mockNavigate,
 }));
 
-describe("SubmissionBar", () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
+vi.mock("utils/other/zip", () => ({
+  createZipFile: () => mockCreateZip,
+}));
 
-    mockedUseStore.mockImplementation(
-      (selector?: Parameters<typeof useStore>[0]) => {
-        if (selector) {
+const report = {
+  id: "mock-report-id",
+  type: "RHTP",
+  state: "PA",
+};
+
+describe("SubmissionBar", () => {
+  describe("Submission is enabled", () => {
+    beforeEach(() => {
+      vi.clearAllMocks();
+
+      mockedUseStore.mockImplementation(
+        (selector?: Parameters<typeof useStore>[0]) => {
+          if (selector) {
+            return {
+              submittable: true,
+            };
+          }
           return {
-            sections: [],
-            submittable: true,
+            ...mockStateUserStore,
+            report,
+            setModalComponent: mockSetModalComponent,
+            setModalOpen: mockSetModalOpen,
           };
         }
-        return {
-          ...mockStateUserStore,
-          report: {
-            id: "mock-report-id",
-            type: "RHTP",
-            state: "PA",
-          },
-          setModalComponent: mockSetModalComponent,
-        };
-      }
-    );
+      );
+      render(<SubmissionBar />);
+    });
+    test("SubmissionBar renders", () => {
+      expect(screen.getByRole("button", { name: "ZIP Files" })).toBeVisible();
+      expect(
+        screen.getByRole("button", { name: "Submit RHTP Report" })
+      ).toBeVisible();
+    });
+    test("Click Submit Report", async () => {
+      const submitBtn = screen.getByRole("button", {
+        name: "Submit RHTP Report",
+      });
+      await userEvent.click(submitBtn);
+      waitFor(() => {
+        expect(mockSetModalOpen).toHaveBeenCalled();
+      });
+    });
+    test("Click Zip Files", async () => {
+      const zipFilesBtn = screen.getByRole("button", { name: "ZIP Files" });
+      await userEvent.click(zipFilesBtn);
+      waitFor(() => {
+        expect(mockCreateZip).toHaveBeenCalled();
+      });
+    });
   });
-  test("SubmissionBar renders", () => {
-    render(<SubmissionBar />);
-    expect(screen.getByRole("button", { name: "ZIP Files" })).toBeVisible();
-    expect(
-      screen.getByRole("button", { name: "Submit RHTP Report" })
-    ).toBeVisible();
+
+  describe("Submission is disabled", () => {
+    beforeEach(() => {
+      vi.clearAllMocks();
+
+      mockedUseStore.mockImplementation(
+        (selector?: Parameters<typeof useStore>[0]) => {
+          if (selector) {
+            return {
+              submittable: false,
+            };
+          }
+          return {
+            ...mockStateUserStore,
+            report,
+            setModalComponent: mockSetModalComponent,
+          };
+        }
+      );
+    });
+    test("should disable the submit button when submittable is false", () => {
+      render(<SubmissionBar />);
+      expect(
+        screen.getByRole("button", { name: "Submit RHTP Report" })
+      ).toBeDisabled();
+    });
   });
 });
