44de14375a6cae13f5ac56ffa590584862ff1cec
Garrett Rabian <garrett.rabian@coforma.io>
2026-04-23 15:08:16 -0400

    allow public subnets

--- deployment/stacks/topics.ts
+++ deployment/stacks/topics.ts
@@ -57,6 +57,7 @@ export function createTopicsComponents(props: CreateTopicsComponentsProps) {
     handler: "handler",
     timeout: Duration.seconds(60),
     retryAttempts: 0,
+    allowPublicSubnet: true,
     ...commonProps,
   });
 
@@ -65,6 +66,7 @@ export function createTopicsComponents(props: CreateTopicsComponentsProps) {
       entry: "services/topics/handlers/deleteTopics.js",
       handler: "handler",
       timeout: Duration.minutes(5),
+      allowPublicSubnet: true,
       ...commonProps,
     });
 
@@ -80,6 +82,7 @@ export function createTopicsComponents(props: CreateTopicsComponentsProps) {
     handler: "handler",
     timeout: Duration.minutes(5),
     retryAttempts: 0,
+    allowPublicSubnet: true,
     ...commonProps,
   });
 
