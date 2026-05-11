97bb7aba1acb73a1e3e24abd03f7d4fbafcec388
Filipp Gorbunov <filippgorb@gmail.com>
2026-05-11 13:00:01 -0700

    correct user internal names

--- services/shared/src/types/users.ts
+++ services/shared/src/types/users.ts
@@ -1,7 +1,7 @@
 export enum UserRoles {
   ADMIN = "mdctrhtp-bor", // "MDCT RHTP Business Owner Representative"
-  APPROVER = "mdctrhtp-appr", // "MDCT RHTP Approver"
-  HELP_DESK = "mdctrhtp-hd", // "MDCT RHTP Help Desk"
+  APPROVER = "mdctrhtp-approver", // "MDCT RHTP Approver"
+  HELP_DESK = "mdctrhtp-help-desk", // "MDCT RHTP Help Desk"
   INTERNAL = "mdctrhtp-internal-user", // "MDCT RHTP Internal User"
   STATE_USER = "mdctrhtp-state-user", // "MDCT RHTP State User",
   PROJECT_OFFICER = "mdctrhtp-project-officer",
