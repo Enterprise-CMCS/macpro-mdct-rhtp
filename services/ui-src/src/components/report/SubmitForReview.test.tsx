cedd449bab49141ea40cb4fb4a3806b5b9639773
Garrett Rabian <garrett.rabian@coforma.io>
2026-05-12 11:37:31 -0400

    submit component and tests

---
+++ services/ui-src/src/components/report/SubmitForReview.test.tsx
@@ -0,0 +1,145 @@
+import { MockedFunction } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { SubmitForReview } from "./SubmitForReview";
+import { putReport, useStore } from "utils";
+import { mockUseStore } from "utils/testing/setupTest";
+import userEvent from "@testing-library/user-event";
+
+vi.mock("utils/state/useStore");
+const mockedUseStore = useStore as unknown as MockedFunction<typeof useStore>;
+
+vi.mock("utils/api/requestMethods/report");
+const mockPutReport = vi.mocked(putReport);
+
+const openAndCompleteFormWithText = async (text: string) => {
+  const modalButton = screen.getByRole("button", { name: "Submit for Review" });
+  await userEvent.click(modalButton);
+  expect(
+    screen.getByRole("heading", { name: "Submit for Review" })
+  ).toBeVisible();
+  const commentBox = screen.getByRole("textbox", { name: "Add Comment" });
+  const submitButton = screen.getByRole("button", {
+    name: "Submit for Review",
+  });
+  expect(commentBox).toBeVisible();
+  expect(submitButton).toBeVisible();
+  if (text) {
+    await userEvent.type(commentBox, text);
+  }
+  await userEvent.click(submitButton);
+};
+
+describe("SubmitForReview component", () => {
+  test("does not render when no report in store", () => {
+    mockedUseStore.mockReturnValue({});
+    const { container } = render(<SubmitForReview />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  test("shows success text when comment already submitted", async () => {
+    const mockStoreWithComment = {
+      ...mockUseStore,
+    };
+    mockStoreWithComment.report!.comments = [
+      {
+        name: "test user",
+        date: new Date().toString(),
+        comment: "ready for review",
+        isInternal: false,
+      },
+    ];
+    mockedUseStore.mockReturnValue(mockStoreWithComment);
+    render(<SubmitForReview />);
+    expect(screen.getByText("Submitted for Review")).toBeVisible();
+  });
+
+  test("appends comment to existing ones", async () => {
+    const mockStoreWithComment = {
+      ...mockUseStore,
+    };
+    mockStoreWithComment.report!.comments = [
+      {
+        name: "test user",
+        date: new Date().toString(),
+        comment: "existing comment",
+        isInternal: false,
+      },
+    ];
+    mockedUseStore.mockReturnValue(mockStoreWithComment);
+    render(<SubmitForReview />);
+
+    await openAndCompleteFormWithText("Please review");
+    expect(mockPutReport).toHaveBeenCalledWith(
+      expect.objectContaining({
+        comments: expect.arrayContaining([
+          expect.objectContaining({
+            name: mockUseStore.user?.full_name,
+            comment: "Please review",
+            isInternal: false,
+          }),
+          expect.objectContaining({
+            name: "test user",
+            comment: "existing comment",
+            isInternal: false,
+          }),
+        ]),
+      })
+    );
+    expect(screen.getByText("Submitted for Review")).toBeVisible();
+  });
+
+  describe("test functionality", () => {
+    beforeEach(() => {
+      vi.clearAllMocks();
+      mockedUseStore.mockReturnValue(mockUseStore);
+      render(<SubmitForReview />);
+    });
+    test("renders", () => {
+      expect(screen.getByText("Ready for Review?")).toBeVisible();
+      expect(
+        screen.getByRole("button", { name: "Submit for Review" })
+      ).toBeVisible();
+    });
+
+    test("can open and close modal", async () => {
+      const modalButton = screen.getByRole("button", {
+        name: "Submit for Review",
+      });
+      await userEvent.click(modalButton);
+      expect(
+        screen.getByRole("heading", { name: "Submit for Review" })
+      ).toBeVisible();
+      expect(
+        screen.getByRole("textbox", { name: "Add Comment" })
+      ).toBeVisible();
+      expect(
+        screen.getByRole("button", { name: "Submit for Review" })
+      ).toBeVisible();
+      const closeButton = screen.getByRole("button", { name: "Close" });
+      await userEvent.click(closeButton);
+      expect(mockPutReport).not.toHaveBeenCalled();
+    });
+
+    test("shows error message for empty comment box", async () => {
+      await openAndCompleteFormWithText("");
+      expect(screen.getByText("A response is required.")).toBeVisible();
+      expect(mockPutReport).not.toHaveBeenCalled();
+    });
+
+    test("open and submit modal", async () => {
+      await openAndCompleteFormWithText("Please review");
+      expect(mockPutReport).toHaveBeenCalledWith(
+        expect.objectContaining({
+          comments: expect.arrayContaining([
+            expect.objectContaining({
+              name: mockUseStore.user?.full_name,
+              comment: "Please review",
+              isInternal: false,
+            }),
+          ]),
+        })
+      );
+      expect(screen.getByText("Submitted for Review")).toBeVisible();
+    });
+  });
+});
---
+++ services/ui-src/src/components/report/SubmitForReview.tsx
@@ -0,0 +1,114 @@
+import { Button, Stack, Text } from "@chakra-ui/react";
+import { TextField } from "@cmsgov/design-system";
+import { AlertTypes, ReportComment } from "@rhtp/shared";
+import { Alert } from "components/alerts/Alert";
+import { Modal } from "components/modals/Modal";
+import { useEffect, useState } from "react";
+import { putReport, useStore } from "utils";
+
+export const SubmitForReview = () => {
+  const [displayValue, setDisplayValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmittedForReview, setIsSubmittedForReview] = useState(false);
+  const { full_name, userIsEndUser } = useStore()?.user ?? {};
+  const { report } = useStore();
+
+  if (!report) {
+    return null;
+  }
+
+  // TODO: update to more accurately reflect when submit for review is published
+  useEffect(() => {
+    if (report.comments && report.comments.length > 0) {
+      setIsSubmittedForReview(true);
+    }
+  }, []);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setDisplayValue(value);
+  };
+
+  const onSubmit = async () => {
+    const commentsEmpty = displayValue.trim() === "";
+    if (commentsEmpty) {
+      setErrorMessage("A response is required.");
+      return;
+    }
+    setIsSubmitting(true);
+
+    const comment: ReportComment = {
+      name: full_name || "CMS State User",
+      date: new Date().toString(),
+      comment: displayValue,
+      isInternal: false,
+    };
+
+    if (report?.comments) {
+      report.comments.push(comment);
+    } else {
+      report.comments = [comment];
+    }
+
+    await putReport(report);
+    setDisplayValue("");
+    setErrorMessage("");
+    setIsSubmittedForReview(true);
+    setModalOpen(false);
+    setIsSubmitting(false);
+  };
+
+  return (
+    <Stack>
+      {isSubmittedForReview ? (
+        <Alert title={"Submitted for Review"} status={AlertTypes.SUCCESS}>
+          Instructions to come
+        </Alert>
+      ) : null}
+      <Text fontSize="heading_md" fontWeight="heading_md">
+        Ready for Review?
+      </Text>
+      <Text fontSize="body_md">
+        [Instructions about what happens during review]
+      </Text>
+      <Button
+        variant="outline"
+        width="fit-content"
+        marginTop="1rem"
+        onClick={() => setModalOpen(true)}
+        disabled={!userIsEndUser}
+      >
+        Submit for Review
+      </Button>
+      <Modal
+        modalDisclosure={{
+          isOpen: isModalOpen,
+          onClose: () => {
+            setModalOpen(false);
+            setErrorMessage("");
+          },
+        }}
+        onConfirmHandler={onSubmit}
+        submitting={isSubmitting}
+        content={{
+          heading: "Submit for Review",
+          subheading: "{More instructions to come}",
+          actionButtonText: "Submit for Review",
+          closeButtonText: "Cancel",
+        }}
+      >
+        <TextField
+          name={"comment"}
+          label={"Add Comment"}
+          onChange={onChange}
+          value={displayValue}
+          errorMessage={errorMessage}
+          multiline
+          rows={3}
+        />
+      </Modal>
+    </Stack>
+  );
+};
