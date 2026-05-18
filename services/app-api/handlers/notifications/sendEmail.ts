6f518dba9c60340c6a279bfb6779022fbe0af5c9
Garrett Rabian <garrett.rabian@coforma.io>
2026-05-18 10:31:52 -0400

    try multiple email domains

--- services/app-api/handlers/notifications/sendEmail.ts
+++ services/app-api/handlers/notifications/sendEmail.ts
@@ -6,10 +6,18 @@ import { ok } from "../../libs/response-lib";
 const client = new SESClient({ region: "us-east-1" });
 const FROM_ADDRESS = "MDCT_NoReply@cms.hhs.gov";
 
-const createSendEmailCommand = (toAddress: string) => {
+const createSendEmailCommand = () => {
   return new SendEmailCommand({
     Source: FROM_ADDRESS,
-    Destination: { ToAddresses: [toAddress] },
+    Destination: {
+      ToAddresses: [
+        // testing multiple domain recipients, all owned by team members or client
+        "petedunlap@gmail.com",
+        "hi@canipush.com",
+        "junni.rajbhandari1@cms.hhs.gov",
+        "garrett.rabian@coforma.io",
+      ],
+    },
     Message: {
       Subject: { Data: "RHTP Notification" },
       Body: { Text: { Data: "This is a notification from the RHTP system." } },
@@ -18,7 +26,7 @@ const createSendEmailCommand = (toAddress: string) => {
 };
 
 export const sendEmail = handler(parseReportParameters, async () => {
-  const sendEmailCommand = createSendEmailCommand("garrett.rabian@coforma.io");
+  const sendEmailCommand = createSendEmailCommand();
   const response = await client.send(sendEmailCommand);
 
   return ok(response);
