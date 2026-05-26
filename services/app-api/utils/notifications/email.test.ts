8d948dba49241e4b8f009dc65c3edae388e6443c
Garrett Rabian <garrett.rabian@coforma.io>
2026-05-26 15:06:59 -0400

    fixup test

--- services/app-api/utils/notifications/email.test.ts
+++ services/app-api/utils/notifications/email.test.ts
@@ -13,8 +13,16 @@ describe("sendEmail", () => {
     vi.clearAllMocks();
   });
 
-  test("should issue a send email command", async () => {
+  test("should not issue a send email command when no email in report", async () => {
     await sendEmail(validReport);
+    expect(sesLib.sendSesEmail).not.toHaveBeenCalled();
+  });
+
+  test("should issue a send email command when email in report", async () => {
+    // any type so it doesn't complain about accessing .answer on generic PageElement
+    const reportWithEmail: any = structuredClone(validReport);
+    reportWithEmail.pages[1].elements[2].answer = "test@email.com";
+    await sendEmail(reportWithEmail);
     expect(sesLib.sendSesEmail).toHaveBeenCalledTimes(1);
   });
 });
