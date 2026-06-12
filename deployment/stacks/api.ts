b1ee7c435abbda85409c6905888b5792aec04c7f
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2026-06-12 09:57:45 -0400

    [CMDCT-6086 pt. 2] Alert a teammate when an email bounces (verify in main) (#222)
    * temp enable email
    
    * change domain to not conflict
    
    * try adding config set
    
    * try getting notified
    
    * fix name conflict
    
    * change sender
    
    * add config sent and sns topic and subscriber to test email bounce notifications

--- deployment/stacks/api.ts
+++ deployment/stacks/api.ts
@@ -6,6 +6,7 @@ import {
   aws_s3 as s3,
   aws_ec2 as ec2,
   aws_ses as ses,
+  aws_sns as sns,
   aws_iam as iam,
   CfnOutput,
   Duration,
@@ -67,11 +68,48 @@ export function createApiComponents(props: CreateApiComponentsProps) {
     resources: ["*"],
   });
   if (!isDev) {
+    const topic = new sns.Topic(scope, `${project}-${stage}-failedEmailTopic`);
+    new sns.Subscription(
+      scope,
+      `${project}-${stage}-failed-email-subscription`,
+      {
+        topic: sns.Topic.fromTopicArn(
+          scope,
+          `${project}-${stage}-failed-email-topic`,
+          topic.topicArn
+        ),
+        endpoint: "garrett.rabian@coforma.io",
+        protocol: sns.SubscriptionProtocol.EMAIL,
+      }
+    );
+
+    const configSet = new ses.ConfigurationSet(
+      scope,
+      `${project}-${stage}-email-configuration-set`,
+      {
+        sendingEnabled: true,
+        configurationSetName: `${project}-${stage}-email-configuration-set`,
+        reputationMetrics: true,
+      }
+    );
+
+    configSet.addEventDestination("sns", {
+      destination: ses.EventDestination.snsTopic(topic),
+      configurationSetEventDestinationName: `${project}-${stage}-email-topic`,
+      enabled: true,
+      events: [
+        ses.EmailSendingEvent.REJECT,
+        ses.EmailSendingEvent.BOUNCE,
+        ses.EmailSendingEvent.COMPLAINT,
+      ],
+    });
+
     const senderIdentity = new ses.EmailIdentity(
       scope,
       "SenderDomainIdentity",
       {
         identity: ses.Identity.domain("cms.hhs.gov"),
+        configurationSet: configSet,
       }
     );
 
