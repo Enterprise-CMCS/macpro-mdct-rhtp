535b0fe3c3eb90773b61e79da7ff292a37f506e5
Garrett Rabian <garrett.rabian@coforma.io>
2026-06-15 11:20:00 -0400

    notifications page ui scaffolding

--- services/ui-src/src/components/menus/AdminMenu.test.tsx
+++ services/ui-src/src/components/menus/AdminMenu.test.tsx
@@ -1,8 +1,17 @@
+import { MockedFunction } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { AdminMenu } from "components";
+import { useStore } from "utils";
 import { testA11y } from "utils/testing/commonTests";
 import { RouterWrappedComponent } from "utils/testing/mockRouter";
+import {
+  mockAdminUserStore,
+  mockApproverUserStore,
+} from "utils/testing/setupTest";
+
+vi.mock("utils/state/useStore");
+const mockedUseStore = useStore as unknown as MockedFunction<typeof useStore>;
 
 const menuComponent = (
   <RouterWrappedComponent>
@@ -11,14 +20,40 @@ const menuComponent = (
 );
 
 describe("<Menu />", () => {
+  beforeEach(() => {
+    mockedUseStore.mockReturnValue(mockAdminUserStore);
+  });
   test("should contain a working link to the banner editor", async () => {
     render(menuComponent);
     await userEvent.click(screen.getByRole("button", { name: "admin menu" }));
 
-    const bannerButton = screen.getByText("Banner Editor");
+    const bannerButton = screen.getByRole("button", { name: "Banner Editor" });
     await userEvent.click(bannerButton);
 
     expect(window.location.pathname).toEqual("/admin");
+  });
+
+  test("should not show notifications link for non-approver user", async () => {
+    render(menuComponent);
+    await userEvent.click(screen.getByRole("button", { name: "admin menu" }));
+
+    const notificationsButton = screen.queryByRole("button", {
+      name: "Notifications",
+    });
+    expect(notificationsButton).not.toBeInTheDocument();
+  });
+
+  test("should contain a working link to the notifications editor for approver user", async () => {
+    mockedUseStore.mockReturnValue(mockApproverUserStore);
+    render(menuComponent);
+    await userEvent.click(screen.getByRole("button", { name: "admin menu" }));
+
+    const notificationsButton = screen.getByRole("button", {
+      name: "Notifications",
+    });
+    await userEvent.click(notificationsButton);
+
+    expect(window.location.pathname).toEqual("/notifications");
   });
 
   testA11y(menuComponent);
