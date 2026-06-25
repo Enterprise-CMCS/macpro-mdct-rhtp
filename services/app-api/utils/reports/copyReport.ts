b75cfd9005f96bfc5c72db8e5e2d1caa5e120762
ailZhou <aileen.zhou@coforma.io>
2026-06-25 09:50:04 -0400

    clear answer

--- services/app-api/utils/reports/copyReport.ts
+++ services/app-api/utils/reports/copyReport.ts
@@ -60,6 +60,8 @@ const copyAnswer = (
         );
       } else if (newElement.id === "use-of-funds-attachment") {
         newElement.answer = [];
+      } else if (newElement.id === "initiative-narrative") {
+        newElement.answer = [];
       } else {
         newElement.answer = oldElement.answer;
       }
