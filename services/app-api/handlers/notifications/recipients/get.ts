87af995677d91345921c1d6d979a6cb965bfbe0f
Garrett Rabian <garrett.rabian@coforma.io>
2026-06-30 09:23:28 -0400

    add permissions check on get recipients

--- services/app-api/handlers/notifications/recipients/get.ts
+++ services/app-api/handlers/notifications/recipients/get.ts
@@ -1,10 +1,21 @@
 import { handler } from "../../../libs/handler-lib";
-import { ok } from "../../../libs/response-lib";
+import { forbidden, ok } from "../../../libs/response-lib";
 import { emptyParser } from "../../../libs/param-lib";
 import { scanAllRecipients } from "../../../storage/notificationRecipients";
+import { canModifyNotificationRecipients } from "../../../utils/authorization";
+import { error } from "../../../utils/constants";
 
-export const getNotificationRecipients = handler(emptyParser, async () => {
-  const recipients = await scanAllRecipients();
+export const getNotificationRecipients = handler(
+  emptyParser,
+  async (request) => {
+    const user = request.user;
 
-  return ok(recipients);
-});
+    // only users who can modify recipients may view all of them
+    if (!canModifyNotificationRecipients(user)) {
+      return forbidden(error.UNAUTHORIZED);
+    }
+    const recipients = await scanAllRecipients();
+
+    return ok(recipients);
+  }
+);
