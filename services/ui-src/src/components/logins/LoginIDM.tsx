32447c1c7c7805f539b25760fce81a385bd71127
Garrett Rabian <garrett.rabian@coforma.io>
2026-07-01 12:06:59 -0400

    check and nav to stored url on login

--- services/ui-src/src/components/logins/LoginIDM.tsx
+++ services/ui-src/src/components/logins/LoginIDM.tsx
@@ -1,17 +1,23 @@
 import { useContext } from "react";
-import { UserContext } from "utils";
+import { getReturnUrl, UserContext } from "utils";
 import { Box, Button, Heading } from "@chakra-ui/react";
+import { useNavigate } from "react-router";
 
 export const LoginIDM = () => {
-  const context = useContext(UserContext);
-  const { loginWithIDM } = context;
+  const navigate = useNavigate();
+  const { loginWithIDM } = useContext(UserContext);
+
+  const handleLogin = () => {
+    loginWithIDM();
+    navigate(getReturnUrl());
+  };
 
   return (
     <Box sx={sx.root}>
       <Heading as="h2" fontSize="heading_xl" sx={sx.heading}>
         Log In with IDM
       </Heading>
-      <Button sx={sx.button} onClick={loginWithIDM}>
+      <Button sx={sx.button} onClick={handleLogin}>
         Log In with IDM
       </Button>
     </Box>
