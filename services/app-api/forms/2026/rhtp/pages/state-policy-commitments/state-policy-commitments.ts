8c96b8f3c99e87ac0bfe32b52e10d2f84a599135
ailZhou <aileen.zhou@coforma.io>
2026-07-13 11:10:28 -0400

    update some required an quarterly report fields

--- services/app-api/forms/2026/rhtp/pages/state-policy-commitments/state-policy-commitments.ts
+++ services/app-api/forms/2026/rhtp/pages/state-policy-commitments/state-policy-commitments.ts
@@ -47,7 +47,7 @@ const cmsStatusEvaluation = (label: string): DropdownTemplate => {
     id: "cms-status-evaluation",
     label: "CMS Status Evaluation",
     options: dropdownOptions,
-    required: true,
+    required: false,
     answer: cmsEvaluationStatusDefault.value,
     editByRole: [UserRoles.ADMIN, UserRoles.PROJECT_OFFICER],
   };
@@ -128,7 +128,7 @@ export const buildStatePolicyCommitments = (
       type: ElementType.AccordionGroup,
       id: "state-policy-commitments-group",
       accordions: [...buildCommitments(state)],
-      required: true,
+      required: false,
     },
   ],
 });
