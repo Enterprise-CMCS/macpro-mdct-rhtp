31911b1211b877f777eeff5af4d8368d51cc76e8
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2026-07-13 09:35:01 -0400

    [CMDCT-6014] zip api refactor (#276)
    * cleaning up dropdowns and pages
    
    * it downloads!
    
    * better naming and consolidating functions
    
    * front end clean up
    
    * add a unit test
    
    * Create ExportedZipPage.test.tsx
    
    * forgot to use file name when generating file
    
    * Update ExportedZipPage.test.tsx
    
    * remove confusing API stuff
    
    * revert some code and fix an error
    
    * Update Multiselect.tsx
    
    * revert and simplify some more
    
    * refactor zip api for extensibility
    
    ---------
    
    Co-authored-by: ailZhou <aileen.zhou@coforma.io>

--- services/app-api/handlers/uploads/zip.ts
+++ services/app-api/handlers/uploads/zip.ts
@@ -1,4 +1,3 @@
-import s3 from "../../libs/s3-lib";
 import { handler } from "../../libs/handler-lib";
 import { parseFileUploadDownloadParameters } from "../../libs/param-lib";
 import { ok } from "../../libs/response-lib";
@@ -7,18 +6,12 @@ import { ReportType, StateAbbr } from "@rhtp/shared";
 import { LambdaClient } from "@aws-sdk/client-lambda";
 import JSZip from "jszip";
 import {
-  sortElementsForZip,
-  getInitativeFiles,
-  getAccordionFiles,
-  getSustainabilityAndHighlightFiles,
+  addReportFilesToZip,
+  formatS3ReportZipKey,
 } from "../../utils/zips/buildZip";
-
-import { getPSURL, zipBuffer, runZipWorker } from "../../utils/zips/polling";
+import { getPSURL, zipBuffer, startZipWorker } from "../../utils/zips/polling";
 
 const lambdaClient = new LambdaClient({ region: "us-east-1" });
-
-const S3ZipKey = (reportType: string, state: string, id: string) =>
-  `zips/${reportType}/${state}/${id}.zip`;
 
 interface ZipWorkerEvent {
   reportType: ReportType;
@@ -30,13 +23,7 @@ export const triggerZipGeneration = handler(
   parseFileUploadDownloadParameters,
   async (request) => {
     const { state, reportType, id } = request.parameters;
-
-    await runZipWorker(lambdaClient, S3ZipKey(reportType, state, id), {
-      reportType,
-      state,
-      id,
-    });
-
+    await startZipWorker(lambdaClient, reportType, state, id);
     return ok({ status: "pending" });
   }
 );
@@ -45,55 +32,16 @@ export const getZipStatus = handler(
   parseFileUploadDownloadParameters,
   async (request) => {
     const { state, reportType, id } = request.parameters;
-    const key = S3ZipKey(reportType, state, id);
-
-    const report = await getReport(reportType, state, id);
-    return await getPSURL(
-      key,
-      `attachment; filename=RHTP_${state}_${report?.subTypeKey}.zip`
-    );
+    return await getPSURL(reportType, state, id);
   }
 );
 
 export const zipWorker = async (event: ZipWorkerEvent) => {
   const { reportType, state, id } = event;
-
   const report = await getReport(reportType, state, id);
-
   if (!report) return;
 
-  const sortedElements = sortElementsForZip(report);
-  const zipFolders = [
-    {
-      name: "Initiatives",
-      files: getInitativeFiles(sortedElements?.initiative),
-    },
-    {
-      name: "State Policy Commitments",
-      files: getAccordionFiles(sortedElements?.accordions ?? []),
-    },
-    {
-      name: "Sustainability and Highlights",
-      files: getSustainabilityAndHighlightFiles(sortedElements?.area ?? []),
-    },
-  ];
-
   const zip = new JSZip();
-  for (const folder of zipFolders) {
-    for (const file of folder.files) {
-      const item = await s3.getObject({
-        Bucket: process.env.attachmentsBucketName,
-        Key: `${reportType}/${state}/${id}/${file?.fileId}`,
-      });
-      const bytes = await item.Body?.transformToByteArray();
-      if (bytes && file?.name) {
-        zip.file(
-          `${state}/${report?.subType.toLowerCase()}/${folder.name}/${file.name}`,
-          bytes
-        );
-      }
-    }
-  }
-
-  await zipBuffer(S3ZipKey(reportType, state, id), zip);
+  await addReportFilesToZip(report, zip);
+  await zipBuffer(formatS3ReportZipKey(reportType, state, id), zip);
 };
