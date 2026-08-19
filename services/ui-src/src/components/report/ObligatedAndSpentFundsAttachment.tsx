d9882d852c956120ead0c054b9115423eed2e073
ailZhou <aileen.zhou@coforma.io>
2026-08-19 13:49:34 -0400

    swap obligated and spent to multiple files

--- services/ui-src/src/components/report/ObligatedAndSpentFundsAttachment.tsx
+++ services/ui-src/src/components/report/ObligatedAndSpentFundsAttachment.tsx
@@ -10,6 +10,7 @@ import addIcon from "assets/icons/add/icon_add_blue.svg";
 import addGray from "assets/icons/add/icon_add_gray.svg";
 import { bytesToKiloBytes, useStore } from "utils";
 import { UploadDrawer } from "components/drawers/UploadDrawer";
+import { Dropdown as CmsdsDropdownField } from "@cmsgov/design-system";
 import {
   uploadListRender,
   downloadFile,
@@ -17,7 +18,7 @@ import {
 } from "utils/other/fileUtils";
 import { Modal } from "components/modals/Modal";
 import { Alert } from "components/alerts/Alert";
-import { notAnsweredText } from "../../constants";
+import { budgetPeriodFilterOptions, notAnsweredText } from "../../constants";
 
 export const ObligatedAndSpentFundsAttachmentElement = (
   props: PageElementProps<ObligatedAndSpentFundsAttachmentTemplate>
@@ -29,13 +30,23 @@ export const ObligatedAndSpentFundsAttachmentElement = (
   const { id, state, type: reportType } = report!;
   const { answer, label } = element;
   const files = answer ?? [];
+  const [selectedFile, setSelectedFile] = useState<UploadListProp>();
+  const [budgetPeriod, setBudgetPeriod] = useState<string>("");
 
   const saveToReport = (newFiles: UploadListProp[]) => {
-    updateElement({ answer: newFiles });
+    const selectedBudget = budgetPeriodFilterOptions.find(
+      (opt) => opt.value == budgetPeriod
+    );
+    const modifiedFiles = newFiles.map((file) => ({
+      ...file,
+      label: selectedBudget?.label,
+    }));
+    updateElement({ answer: [...files, ...modifiedFiles] });
   };
 
-  const onDeleteModalOpen = () => {
+  const onDeleteModalOpen = (file: UploadListProp) => {
     setDeleteModalOpen(true);
+    setSelectedFile(file);
   };
 
   const onDeleteModalClose = () => {
@@ -43,13 +54,36 @@ export const ObligatedAndSpentFundsAttachmentElement = (
   };
 
   const onRemove = () => {
-    updateElement({ answer: [] });
-    removeFile(reportType, state, id, files[0]);
+    if (!selectedFile) return;
+    const newFiles = files.filter((file) => file.fileId != selectedFile.fileId);
+    updateElement({ answer: newFiles });
+    removeFile(reportType, state, id, selectedFile);
     onDeleteModalClose();
   };
 
-  const isDisabled = () => {
-    return disabled || files.length > 0;
+  const handleBudgetPeriodChange = (evt: { target: { value: string } }) => {
+    setBudgetPeriod(evt.target.value);
+  };
+
+  const getNotification = () => {
+    const selectedBudget = budgetPeriodFilterOptions.find(
+      (opt) => opt.value == budgetPeriod
+    );
+    const instruction =
+      budgetPeriod === "All" || !budgetPeriod
+        ? {
+            type: AlertTypes.WARNING,
+            text: "Select a budget period to enable upload.",
+          }
+        : {
+            type: AlertTypes.INFO,
+            text: `Attaching to: ${selectedBudget?.label}`,
+          };
+
+    return {
+      instruction: instruction,
+      success: `${selectedBudget?.label}`,
+    };
   };
 
   return (
@@ -59,8 +93,8 @@ export const ObligatedAndSpentFundsAttachmentElement = (
         onClick={() => {
           setModalOpen(true);
         }}
-        disabled={isDisabled()}
-        leftIcon={<Image src={isDisabled() ? addGray : addIcon} />}
+        disabled={disabled}
+        leftIcon={<Image src={disabled ? addGray : addIcon} />}
       >
         Add Obligated and Spent Funds
       </Button>
@@ -71,12 +105,20 @@ export const ObligatedAndSpentFundsAttachmentElement = (
             setModalOpen(false);
           },
         }}
+        selections={
+          <CmsdsDropdownField
+            name="budgetPeriodFilter"
+            label="Filter by Budget Period"
+            value={budgetPeriod}
+            onChange={handleBudgetPeriodChange}
+            options={budgetPeriodFilterOptions}
+          />
+        }
         modalHeading={"Add Obligated and Spent Funds"}
         answer={files}
         saveToReport={saveToReport}
         deleteFromReport={onRemove}
-        multiple={false}
-        notification={{ success: "Obligated and Spent Funds" }}
+        notification={getNotification()}
       ></UploadDrawer>
       {files.length > 0 && (
         <Heading as="h2" fontWeight="bold" marginBottom="-0.5rem">
@@ -116,10 +158,9 @@ export const ObligatedAndSpentFundsAttachmentElement = (
           reportType,
           state,
           id,
-          files,
+          selectedFile ? [selectedFile] : [],
           undefined,
-          downloadFile,
-          disabled
+          downloadFile
         )}
       </Modal>
     </Fragment>
