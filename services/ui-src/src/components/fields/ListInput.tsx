dbe11b38cc246614ba65aa9d4ec4e4c8fa4b6814
Garrett Rabian <garrett.rabian@coforma.io>
2026-08-20 11:12:20 -0400

    improve legend and button accessibility in list input

--- services/ui-src/src/components/fields/ListInput.tsx
+++ services/ui-src/src/components/fields/ListInput.tsx
@@ -79,7 +79,9 @@ export const ListInput = (props: PageElementProps<ListInputTemplate>) => {
 
   return (
     <fieldset key="list-input-field">
-      <Label fieldId={id}>{optionalTag(element)}</Label>
+      <Label component={"legend"} fieldId={id}>
+        {optionalTag(element)}
+      </Label>
       <Hint id={id}>{helperText}</Hint>
       {displayValue.map((field, index) => (
         <HStack
@@ -101,7 +103,7 @@ export const ListInput = (props: PageElementProps<ListInputTemplate>) => {
             variant="unstyled"
             onClick={() => onRemoveHandler(index)}
             disabled={isDisabled()}
-            aria-label={`Remove ${field}`}
+            aria-label={`Remove ${fieldLabel} ${index + 1}`}
             leftIcon={<Image src={cancelPrimary} alt="Remove icon" />}
           />
         </HStack>
