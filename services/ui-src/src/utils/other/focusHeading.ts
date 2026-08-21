3a76d52b87eec5c43fd0acdf6d0ca3b04e9f54de
ailZhou <aileen.zhou@coforma.io>
2026-08-21 13:50:29 -0400

    clean up and unit test

--- services/ui-src/src/utils/other/focusHeading.ts
+++
@@ -1,8 +0,0 @@
-export const focusHeading = () => {
-  const target =
-    document.querySelector("h1") ?? document.querySelector("#main-content");
-
-  target?.setAttribute("tabindex", "-1");
-  target?.focus();
-  window.scrollTo(0, 0);
-};
